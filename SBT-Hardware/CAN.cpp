238e95fb7c76085041bcf1e2bd0be9ba308b2272
DarKreter <darkreter@gmail.com>
2021-05-22 21:47:02 +0200

    Create CAN files

---
+++ SBT-Hardware/CAN.cpp
@@ -0,0 +1,5 @@
+//
+// Created by darkr on 22.05.2021.
+//
+
+#include "CAN.hpp"
---
+++ SBT-Hardware/CAN.hpp
@@ -0,0 +1,15 @@
+//
+// Created by darkr on 22.05.2021.
+//
+
+#ifndef F1XX_PROJECT_TEMPLATE_CAN_HPP
+#define F1XX_PROJECT_TEMPLATE_CAN_HPP
+
+
+class CAN
+{
+
+};
+
+
+#endif //F1XX_PROJECT_TEMPLATE_CAN_HPP
