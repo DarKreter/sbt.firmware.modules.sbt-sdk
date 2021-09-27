6e84ca8a78fc7443e837156830ae652f4bef2262
DarKreter <darkreter@gmail.com>
2021-09-27 21:08:06 +0200

    Updated parameters

--- SBT-Hardware/CAN_ID.hpp
+++ SBT-Hardware/CAN_ID.hpp
@@ -19,60 +19,43 @@ enum class BoxId : uint32_t
 
 enum class ParameterId : uint16_t
 {
-
+    //-----------------------------------------------------------------------------------------------------------
+    // NAME                ID            Description                     Type       Unit         Range
+    //-----------------------------------------------------------------------------------------------------------
+    
     /*** BOX 1 ***/
     // KLS
-    actualSpeed = 0x00,    //Actual speed           type: int;  range: 0-600;    unit: rpm;
-    actualMotorCurren,     //Actual motor current   type: int;  range: 0,0 - 400,0; unit: 0,1A;
-    actualBateryVoltage,   //Actual batery voltage  type: int;  range: 0,0 - 180,0; unit: 0,1V;
-    throtleSignal,         //Throtle signal         type: int;  range: "0~255 maps 0~5V";
-    controllerTemperature, //Controller temperature type: int;  unit: *C (offset 40)
-    motorTemperature,      //Controller temperature type: int;  unit: *C (offset 30)
+    kls_speed             = 0x00,   //Actual speed                      int32_t     rpm         0-600
+    kls_current,                    //Actual motor current              int32_t     0,1A        0,0 - 400,0
+    kls_bateryVoltage,              //Actual batery voltage             int32_t     0,1V        0,0 - 180,0
+    kls_throtleSignal,              //Throtle signal                    int32_t      V          0~255 maps 0~5V
+    kls_controllerTemperature,      //Controller temperature            int32_t     *C          (offset 40)
+    kls_motorTemperature,           //Controller temperature            int32_t     *C          (offset 30)
 
     /*** BOX 2 ***/
-    //MPPT 1                        //  ID     Description                    Scale   Type    Unit
-     mppt1_panelPower=0x10,         //  0xEDBC Panel power                    0.01     un32    W
-     mppt1_panelVoltage,            //  0xEDBB Panel voltage                  0.01     un16    V
-     mppt1_panelCurrent,            //  0xEDBD Panel current (*1)             0.1      un16    A
-     mppt1_panelMaximumVoltage,     //  0xEDB8 Panel maximum voltage (*2)     0.01     un16    V .....
-     mppt1_chargerMaximumCurrent,   //  0xEDDF Charger maximum current (*1)   0.01     un16    A
-     mppt1_batteryMaximumCurrent,   //  0xEDF0 Battery maximum current        0.1      un16    A .......
-     mppt1_chargerCurrent,          //  0xEDD7 Charger current (3)            0.1      un16    A
-     mppt1_chargerVoltage,          //  0xEDD5 Charger voltage (3)            0.01     un16    V
-     mppt1_batteryVoltage,          //  0xEDEF Battery voltage (4)            1        un8     V
+    //MPPT 1
+     mppt1_panelPower       = 0x10, //  0xEDBC Panel power              float       W
+     mppt1_panelVoltage,            //  0xEDBB Panel voltage            float       V
+     mppt1_panelCurrent,            //  0xEDBD Panel current (1)        float       A
+     mppt1_chargerCurrent,          //  0xEDD7 Charger current (3)      float       A
 
     //MPPT 2
-     mppt2_panelPower = 0x20,       //  0xEDBC Panel power                    0.01    un32   W
-     mppt2_panelVoltage,            //  0xEDBB Panel voltage                  0.01    un16   V
-     mppt2_panelCurrent,            //  0xEDBD Panel current (1)              0.1     un16   A
-     mppt2_panelMaximumVoltage,     //  0xEDB8 Panel maximum voltage (*2)     0.01    un16   V
-     mppt2_chargerMaximumCurrent,   //  0xEDDF Charger maximum current (*1)   0.01    un16   A
-     mppt2_batteryMaximumCurrent,   //  0xEDF0 Battery maximum current        0.1     un16   A .......
-     mppt2_chargerCurrent,          //  0xEDD7 Charger current (3)            0.1     un16   A
-     mppt2_chargerVoltage,          //  0xEDD5 Charger voltage (3)            0.01    un16   V
-     mppt2_batteryVoltage,          //  0xEDEF Battery voltage (4)            1       un8    V
+     mppt2_panelPower       = 0x20, //  0xEDBC Panel power              float       W
+     mppt2_panelVoltage,            //  0xEDBB Panel voltage            float       V
+     mppt2_panelCurrent,            //  0xEDBD Panel current (1)        float       A
+     mppt2_chargerCurrent,          //  0xEDD7 Charger current (3)      float       A
 
     //MPPT 3
-    mppt3_panelPower = 0x30,       //  0xEDBC Panel power                     0.01    un32   W
-    mppt3_panelVoltage,            //  0xEDBB Panel voltage                   0.01    un16   V
-    mppt3_panelCurrent,            //  0xEDBD Panel current (1)               0.1     un16   A
-    mppt3_panelMaximumVoltage,     //  0xEDB8 Panel maximum voltage (*2)      0.01    un16   V
-    mppt3_chargerMaximumCurrent,   //  0xEDDF Charger maximum current (*1)    0.01    un16   A
-    mppt3_batteryMaximumCurrent,   //  0xEDF0 Battery maximum current         0.1     un16   A .......
-    mppt3_chargerCurrent,          //  0xEDD7 Charger current (3)             0.1     un16   A
-    mppt3_chargerVoltage,          //  0xEDD5 Charger voltage (3)             0.01    un16   V
-    mppt3_batteryVoltage,          //  0xEDEF Battery voltage (4)             1       un8    V
+    mppt3_panelPower        = 0x30, //  0xEDBC Panel power              float       W
+    mppt3_panelVoltage,             //  0xEDBB Panel voltage            float       V
+    mppt3_panelCurrent,             //  0xEDBD Panel current (1)        float       A
+    mppt3_chargerCurrent,           //  0xEDD7 Charger current (3)      float       A
 
     //MPPT 4
-    mppt4_panelPower = 0x40,       //  0xEDBC Panel power                     0.01    un32   W
-    mppt4_panelVoltage,            //  0xEDBB Panel voltage                   0.01    un16   V
-    mppt4_panelCurrent,            //  0xEDBD Panel current (1)               0.1     un16   A
-    mppt4_panelMaximumVoltage,     //  0xEDB8 Panel maximum voltage (*2)      0.01    un16   V
-    mppt4_chargerMaximumCurrent,   //  0xEDDF Charger maximum current (*1)    0.01    un16   A
-    mppt4_batteryMaximumCurrent,   //  0xEDF0 Battery maximum current         0.1     un16   A .......
-    mppt4_chargerCurrent,          //  0xEDD7 Charger current (3)             0.1     un16   A
-    mppt4_chargerVoltage,          //  0xEDD5 Charger voltage (3)             0.01    un16   V
-    mppt4_batteryVoltage,          //  0xEDEF Battery voltage (4)             1       un8    V
+    mppt4_panelPower        = 0x40, //  0xEDBC Panel power              float       W
+    mppt4_panelVoltage,             //  0xEDBB Panel voltage            float       V
+    mppt4_panelCurrent,             //  0xEDBD Panel current (1)        float       A
+    mppt4_chargerCurrent,           //  0xEDD7 Charger current (3)      float       A
 
 
     //Temp&higro
@@ -81,53 +64,105 @@ enum class ParameterId : uint16_t
 
     /*** BOX 3 ***/
     //Bat (LIFePO4) 1
-    bat1_chargingCurrent = 0x60, //Charging Current      type: int;
-    bat1_dischargingCurrent,     //Discharging Current   type: int;
-    bat1_state,                  ///State register       type: int;
-    bat1_chargeLevelPercentage,  //Charge Level in %     type: int;
-    bat1_chargeLevelAh,          //Charge Level in Ah    type: int;
-    bat1_capacity,               //Capacity              type: int;
-    bat1_cellVoltage1,           //Cell Voltage  1       type: int; //TODO(rradmoski): We have 14 cels - how to fit it concept is needed
-    bat1_cellVoltage2,           //Cell Voltage  2       type: int;
-    bat1_cellVoltage3,           //Cell Voltage  3       type: int;
+    bat1_chargingCurrent    = 0x60, //Charging Current                  int32_t
+    bat1_dischargingCurrent,        //Discharging Current               int32_t
+    bat1_state,                     //State register                    int32_t
+    bat1_chargeLevelPercentage,     //Charge Level in %                 int32_t
+    bat1_chargeLevelAh,             //Charge Level in Ah                int32_t
+    bat1_capacity,                  //Capacity                          int32_t
+    bat1_voltage,
+    bat1_power,
+    bat1_cellVoltage1,              //Cell Voltage  1                   int32_t
+    bat1_cellVoltage2,              //Cell Voltage  2                   int32_t
+    bat1_cellVoltage3,              //Cell Voltage  3                   int32_t
+    bat1_cellVoltage4,              //Cell Voltage  4                   int32_t
+    bat1_cellVoltage5,              //Cell Voltage  5                   int32_t
+    bat1_cellVoltage6,              //Cell Voltage  6                   int32_t
+    bat1_cellVoltage7,              //Cell Voltage  7                   int32_t
+    bat1_cellVoltage8,              //Cell Voltage  8                   int32_t
+    bat1_cellVoltage9,              //Cell Voltage  9                   int32_t
+    bat1_cellVoltage10,             //Cell Voltage  10                  int32_t
+    bat1_cellVoltage11,             //Cell Voltage  11                  int32_t
+    bat1_cellVoltage12,             //Cell Voltage  12                  int32_t
+    bat1_cellVoltage13,             //Cell Voltage  13                  int32_t
+    bat1_cellVoltage14,             //Cell Voltage  14                  int32_t
 
     //Bat (LIFePO4) 2
-    bat2_chargingCurrent = 0x70, //Charging Current      type: int;
-    bat2_dischargingCurrent,     //Discharging Current   type: int;
-    bat2_state,                  ///State register       type: int;
-    bat2_chargeLevelPercentage,  //Charge Level in %     type: int;
-    bat2_chargeLevelAh,          //Charge Level in Ah    type: int;
-    bat2_capacity,               //Capacity              type: int;
-    bat2_cellVoltage1,           //Cell Voltage  1       type: int; //TODO(rradmoski): We have 14 cels - how to fit it concept is needed
-    bat2_cellVoltage2,           //Cell Voltage  2       type: int;
-    bat2_cellVoltage3,           //Cell Voltage  3       type: int;
+    bat2_chargingCurrent    = 0x80, //Charging Current                  int32_t
+    bat2_dischargingCurrent,        //Discharging Current               int32_t
+    bat2_state,                     //State register                    int32_t
+    bat2_chargeLevelPercentage,     //Charge Level in %                 int32_t
+    bat2_chargeLevelAh,             //Charge Level in Ah                int32_t
+    bat2_capacity,                  //Capacity                          int32_t
+    bat2_voltage,
+    bat2_power,
+    bat2_cellVoltage1,              //Cell Voltage  1                   int32_t
+    bat2_cellVoltage2,              //Cell Voltage  2                   int32_t
+    bat2_cellVoltage3,              //Cell Voltage  3                   int32_t
+    bat2_cellVoltage4,              //Cell Voltage  4                   int32_t
+    bat2_cellVoltage5,              //Cell Voltage  5                   int32_t
+    bat2_cellVoltage6,              //Cell Voltage  6                   int32_t
+    bat2_cellVoltage7,              //Cell Voltage  7                   int32_t
+    bat2_cellVoltage8,              //Cell Voltage  8                   int32_t
+    bat2_cellVoltage9,              //Cell Voltage  9                   int32_t
+    bat2_cellVoltage10,             //Cell Voltage  10                  int32_t
+    bat2_cellVoltage11,             //Cell Voltage  11                  int32_t
+    bat2_cellVoltage12,             //Cell Voltage  12                  int32_t
+    bat2_cellVoltage13,             //Cell Voltage  13                  int32_t
+    bat2_cellVoltage14,             //Cell Voltage  14                  int32_t
 
     //Bat (LIFePO4) 3
-    bat3_chargingCurrent = 0x80, //Charging Current      type: int;
-    bat3_dischargingCurrent,     //Discharging Current   type: int;
-    bat3_state,                  ///State register       type: int;
-    bat3_chargeLevelPercentage,  //Charge Level in %     type: int;
-    bat3_chargeLevelAh,          //Charge Level in Ah    type: int;
-    bat3_capacity,               //Capacity              type: int;
-    bat3_cellVoltage1,           //Cell Voltage  1       type: int; //TODO(rradmoski): We have 14 cels - how to fit it concept is needed
-    bat3_cellVoltage2,           //Cell Voltage  2       type: int;
-    bat3_cellVoltage3,           //Cell Voltage  3       type: int;
+    bat3_chargingCurrent    = 0xA0, //Charging Current                  int32_t
+    bat3_dischargingCurrent,        //Discharging Current               int32_t
+    bat3_state,                     //State register                    int32_t
+    bat3_chargeLevelPercentage,     //Charge Level in %                 int32_t
+    bat3_chargeLevelAh,             //Charge Level in Ah                int32_t
+    bat3_capacity,                  //Capacity                          int32_t
+    bat3_voltage,
+    bat3_power,
+    bat3_cellVoltage1,              //Cell Voltage  1                   int32_t
+    bat3_cellVoltage2,              //Cell Voltage  2                   int32_t
+    bat3_cellVoltage3,              //Cell Voltage  3                   int32_t
+    bat3_cellVoltage4,              //Cell Voltage  4                   int32_t
+    bat3_cellVoltage5,              //Cell Voltage  5                   int32_t
+    bat3_cellVoltage6,              //Cell Voltage  6                   int32_t
+    bat3_cellVoltage7,              //Cell Voltage  7                   int32_t
+    bat3_cellVoltage8,              //Cell Voltage  8                   int32_t
+    bat3_cellVoltage9,              //Cell Voltage  9                   int32_t
+    bat3_cellVoltage10,             //Cell Voltage  10                  int32_t
+    bat3_cellVoltage11,             //Cell Voltage  11                  int32_t
+    bat3_cellVoltage12,             //Cell Voltage  12                  int32_t
+    bat3_cellVoltage13,             //Cell Voltage  13                  int32_t
+    bat3_cellVoltage14,             //Cell Voltage  14                  int32_t
 
     //Bat (LIFePO4) 4
-    bat4_chargingCurrent = 0x90, //Charging Current      type: int;
-    bat4_dischargingCurrent,     //Discharging Current   type: int;
-    bat4_state,                  ///State register       type: int;
-    bat4_chargeLevelPercentage,  //Charge Level in %     type: int;
-    bat4_chargeLevelAh,          //Charge Level in Ah    type: int;
-    bat4_capacity,               //Capacity              type: int;
-    bat4_cellVoltage1,           //Cell Voltage  1       type: int; //TODO(rradmoski): We have 14 cels - how to fit it concept is needed
-    bat4_cellVoltage2,           //Cell Voltage  2       type: int;
-    bat4_cellVoltage3,           //Cell Voltage  3       type: int;
+    bat4_chargingCurrent    = 0xC0, //Charging Current                  int32_t
+    bat4_dischargingCurrent,        //Discharging Current               int32_t
+    bat4_state,                     //State register                    int32_t
+    bat4_chargeLevelPercentage,     //Charge Level in %                 int32_t
+    bat4_chargeLevelAh,             //Charge Level in Ah                int32_t
+    bat4_capacity,                  //Capacity                          int32_t
+    bat4_voltage,
+    bat4_power,
+    bat4_cellVoltage1,              //Cell Voltage  1                   int32_t
+    bat4_cellVoltage2,              //Cell Voltage  2                   int32_t
+    bat4_cellVoltage3,              //Cell Voltage  3                   int32_t
+    bat4_cellVoltage4,              //Cell Voltage  4                   int32_t
+    bat4_cellVoltage5,              //Cell Voltage  5                   int32_t
+    bat4_cellVoltage6,              //Cell Voltage  6                   int32_t
+    bat4_cellVoltage7,              //Cell Voltage  7                   int32_t
+    bat4_cellVoltage8,              //Cell Voltage  8                   int32_t
+    bat4_cellVoltage9,              //Cell Voltage  9                   int32_t
+    bat4_cellVoltage10,             //Cell Voltage  10                  int32_t
+    bat4_cellVoltage11,             //Cell Voltage  11                  int32_t
+    bat4_cellVoltage12,             //Cell Voltage  12                  int32_t
+    bat4_cellVoltage13,             //Cell Voltage  13                  int32_t
+    bat4_cellVoltage14,             //Cell Voltage  14                  int32_t
 
 
     /*** BOX OTHER ***/
     // AHRS
-    ACCEL_X = 0xA0, // float
+    ACCEL_X                 = 0xE0, // float
     ACCEL_Y,
     ACCEL_Z,
     GYRO_X,
