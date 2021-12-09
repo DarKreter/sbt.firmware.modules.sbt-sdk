953be7291fc8ac73ab579bf7d3cfbdccec12ba68
DarKreter <darkreter@gmail.com>
2021-12-09 20:23:45 +0000

    clang update

--- Config/FreeRTOSConfig.h
+++ Config/FreeRTOSConfig.h
@@ -35,38 +35,38 @@ extern uint32_t SystemCoreClock;
 #define configMESSAGE_BUFFER_LENGTH_TYPE        size_t
 
 /* Memory allocation related definitions. */
-#define configSUPPORT_STATIC_ALLOCATION  0
-#define configSUPPORT_DYNAMIC_ALLOCATION 1
-#define configTOTAL_HEAP_SIZE            10240
-#define configAPPLICATION_ALLOCATED_HEAP 1
+#define configSUPPORT_STATIC_ALLOCATION         0
+#define configSUPPORT_DYNAMIC_ALLOCATION        1
+#define configTOTAL_HEAP_SIZE                   10240
+#define configAPPLICATION_ALLOCATED_HEAP        1
 
 /* Hook function related definitions. */
-#define configUSE_IDLE_HOOK                0
-#define configUSE_TICK_HOOK                0
-#define configCHECK_FOR_STACK_OVERFLOW     0
-#define configUSE_MALLOC_FAILED_HOOK       0
-#define configUSE_DAEMON_TASK_STARTUP_HOOK 0
+#define configUSE_IDLE_HOOK                     0
+#define configUSE_TICK_HOOK                     0
+#define configCHECK_FOR_STACK_OVERFLOW          0
+#define configUSE_MALLOC_FAILED_HOOK            0
+#define configUSE_DAEMON_TASK_STARTUP_HOOK      0
 
 /* Run time and task stats gathering related definitions. */
-#define configGENERATE_RUN_TIME_STATS        0
-#define configUSE_TRACE_FACILITY             0
-#define configUSE_STATS_FORMATTING_FUNCTIONS 0
+#define configGENERATE_RUN_TIME_STATS           0
+#define configUSE_TRACE_FACILITY                0
+#define configUSE_STATS_FORMATTING_FUNCTIONS    0
 
 /* Co-routine related definitions. */
-#define configUSE_CO_ROUTINES           0
-#define configMAX_CO_ROUTINE_PRIORITIES 1
+#define configUSE_CO_ROUTINES                   0
+#define configMAX_CO_ROUTINE_PRIORITIES         1
 
 /* Software timer related definitions. */
-#define configUSE_TIMERS             1
-#define configTIMER_TASK_PRIORITY    3
-#define configTIMER_QUEUE_LENGTH     10
-#define configTIMER_TASK_STACK_DEPTH configMINIMAL_STACK_SIZE
+#define configUSE_TIMERS                        1
+#define configTIMER_TASK_PRIORITY               3
+#define configTIMER_QUEUE_LENGTH                10
+#define configTIMER_TASK_STACK_DEPTH            configMINIMAL_STACK_SIZE
 
 /* Interrupt nesting behaviour configuration. */
-#define configKERNEL_INTERRUPT_PRIORITY      0
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY 5
+#define configKERNEL_INTERRUPT_PRIORITY         0
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    5
 #define configMAX_API_CALL_INTERRUPT_PRIORITY                                  \
-  configMAX_SYSCALL_INTERRUPT_PRIORITY
+    configMAX_SYSCALL_INTERRUPT_PRIORITY
 
 /* Define to trap errors during development. */
 //#define configASSERT( ( x ) ) if( ( x ) == 0 ) vAssertCalled( __FILE__,
@@ -80,23 +80,23 @@ extern uint32_t SystemCoreClock;
 #define configENFORCE_SYSTEM_CALLS_FROM_KERNEL_ONLY            1
 
 /* Optional functions - most linkers will remove unused functions anyway. */
-#define INCLUDE_vTaskPrioritySet            1
-#define INCLUDE_uxTaskPriorityGet           1
-#define INCLUDE_vTaskDelete                 1
-#define INCLUDE_vTaskSuspend                1
-#define INCLUDE_xResumeFromISR              1
-#define INCLUDE_vTaskDelayUntil             1
-#define INCLUDE_vTaskDelay                  1
-#define INCLUDE_xTaskGetSchedulerState      1
-#define INCLUDE_xTaskGetCurrentTaskHandle   1
-#define INCLUDE_uxTaskGetStackHighWaterMark 0
-#define INCLUDE_xTaskGetIdleTaskHandle      0
-#define INCLUDE_eTaskGetState               0
-#define INCLUDE_xEventGroupSetBitFromISR    1
-#define INCLUDE_xTimerPendFunctionCall      1
-#define INCLUDE_xTaskAbortDelay             0
-#define INCLUDE_xTaskGetHandle              0
-#define INCLUDE_xTaskResumeFromISR          1
+#define INCLUDE_vTaskPrioritySet                               1
+#define INCLUDE_uxTaskPriorityGet                              1
+#define INCLUDE_vTaskDelete                                    1
+#define INCLUDE_vTaskSuspend                                   1
+#define INCLUDE_xResumeFromISR                                 1
+#define INCLUDE_vTaskDelayUntil                                1
+#define INCLUDE_vTaskDelay                                     1
+#define INCLUDE_xTaskGetSchedulerState                         1
+#define INCLUDE_xTaskGetCurrentTaskHandle                      1
+#define INCLUDE_uxTaskGetStackHighWaterMark                    0
+#define INCLUDE_xTaskGetIdleTaskHandle                         0
+#define INCLUDE_eTaskGetState                                  0
+#define INCLUDE_xEventGroupSetBitFromISR                       1
+#define INCLUDE_xTimerPendFunctionCall                         1
+#define INCLUDE_xTaskAbortDelay                                0
+#define INCLUDE_xTaskGetHandle                                 0
+#define INCLUDE_xTaskResumeFromISR                             1
 
 /* A header file that defines trace macro can be included here. */
 
